77acb166f8e460e2f9aa905c46e3d97a6d6e4028
valeratrades <v79166789533@gmail.com>
2024-10-19 15:29:45 +0200

    added word rendering

--- src/main.py
+++ src/main.py
@@ -7,14 +7,18 @@ try:
 except ImportError:  # Graceful fallback if IceCream isn't installed.
 	ic = lambda *a: None if not a else (a[0] if len(a) == 1 else a)  # noqa
 
-
 def main():
 	correct_word = "turtle"
 	tries = 9
-	run(correct_word, tries)
+	outcome = run(correct_word, tries)
+	s = "winning" if outcome else "losing"
+	print(f"congratulations on {s}")
 
 
-def run(correct_word: str, tries: int):
+def run(correct_word: str, tries: int) -> bool:
+	"""
+	returns: did player win
+	"""
 	print("game start")
 	observed_correct_guesses = set()
 	win_condition_set = set(correct_word)
@@ -30,15 +34,38 @@ def run(correct_word: str, tries: int):
 			continue
 
 		guess_is_correct = guessed_char in win_condition_set
+
 		if guess_is_correct:
 			observed_correct_guesses.add(guessed_char)
 		else:
 			tries -= 1
 
 		if observed_correct_guesses == win_condition_set:
-			break
+			return True
 
-		ic(correct_word, tries, guessed_char, guess_is_correct, win_condition_set, observed_correct_guesses)
+		rendered_word = render_guessed(correct_word, observed_correct_guesses)
+		s = f"\n{rendered_word}\nguesses left: {tries}\n"
+		print(s)
+
+	return False
+
+def render_guessed(correct_word: str, observed_correct_guesses: set) -> str:
+	"""
+	Ex: ("turtle", {"t", "e"}) -> "t _ _ t _ e"
+	"""
+
+	s = ""
+	i = 0
+	for c in correct_word:
+		if c in observed_correct_guesses:
+			s += c
+		else:
+			s += "_"
+
+		if i != len(correct_word)-1:
+			s += " "
+		i = i+1	
+	return s
 
 
 if __name__ == "__main__":
